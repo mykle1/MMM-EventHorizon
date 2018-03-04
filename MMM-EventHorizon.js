a59475b6fb8d2a1f8733d9c3106c2f7d112ce4c1
Mykle <25211804+mykle1@users.noreply.github.com>
2018-03-04 16:54:11 -0500

    Add files via upload

---
+++ MMM-EventHorizon.js
@@ -0,0 +1,34 @@
+/* Magic Mirror
+ * Module: MMM-EventHorizon
+ * 
+ * By Mykle1 - MIT Licensed
+ * 
+ */
+
+Module.register("MMM-EventHorizon",{
+		
+	defaults: {
+			height:"290px",
+			width:"316px"
+	},
+	
+	getStyles: function() {
+        return ["MMM-EventHorizon.css"];
+    },
+
+
+	
+	getDom: function() {
+		
+		
+		var iframe = document.createElement("IFRAME");
+		iframe.classList.add("iframe");
+		iframe.style = "border:none";
+		iframe.width = this.config.width;
+		iframe.height = this.config.height;
+		type="text/javascript";
+		iframe.src = "http://free.timeanddate.com/countdown/i64zeztz/n179/cf100/cm0/cu4/ct5/cs0/ca0/cr0/ss0/cac33d9ff/cpc000/pct/tcfff/fs100/szw192/szh81/tatTime%20left%20until%20/tac33d9ff/tptWhen%20the%20timer%20runs%20out/tpc33d9ff/matWe%20party%20like%20a%20mofo!/mac33d9ff/mptSay%20whatever%20you%20want/mpc33d9ff/iso2018-03-04T16:50:00/bo2/pd2";
+		return iframe;
+	}
+
+});
---
+++ README.md
@@ -0,0 +1,19 @@
+## MMM-EventHorizon
+
+Simple countdown timer for events.
+
+## Examples
+
+![](images/1.png) ![](images/2.png)
+
+## Installation
+
+* `git clone https://github.com/mykle1/MMM-EventHorizon` into the `~/MagicMirror/modules` directory.
+
+## Config.js entry and options
+
+    {
+            disabled: f,
+            module: 'MMM-EventHorizon',
+            position: 'top_left',
+        },
