9cc496dd13ed6b8ad423a1797e9c97fbab8d136d
Mykle <25211804+mykle1@users.noreply.github.com>
2019-01-14 22:04:38 -0500

    Add files via upload

--- MMM-EventHorizon.js
+++ MMM-EventHorizon.js
@@ -3,27 +3,39 @@
  *
  * By Mykle1 - MIT Licensed
  *
+ * extended by time zone and colors
+ * by @spitzlbergerj January 14, 2019
+ *
  */
 
 Module.register("MMM-EventHorizon",{
 
-	defaults: {
-        size: "small",                        // small, medium or large
-				countUp: "yes",                       // Count up after timer ends
-				date: "2018-07-04",
+    defaults: {
+        timezone: "n37",                      // See Timezone chart at bottom
+        size: "medium",                       // small, medium or large
+        countUp: "yes",                       // Count up after timer ends
+        date: "2019-07-04",                   // YYYY-MM-DD format ONLY
         time: "00:00:01",                     // (HH:MM:SS)    Exact time you want timer to end
         text1: "Describe event here",         // 2 lines of text during timer
         text2: "And here too!",               // 2 lines of text during timer
+        text1Color: "FFFFFF",                 // Hex color codes.	Default = white
+        text2Color: "FFFFFF",                 // Hex color codes.	Default = white
+        timerColor: "FFFFFF",                 // Hex color codes.	Default = white
         endText1: "When timer ends",          // 2 lines of text when timer ends
         endText2: "Say something here",       // 2 lines of text when timer ends
-	},
+        endText1Color: "FFFFFF",              // Hex color codes.	Default = white
+        endText2Color: "FFFFFF",              // Hex color codes.	Default = white
+        timerUpColor: "FFFFFF",               // Hex color codes.	Default = white
+        colorpc: "000",                       // color of the background 000 = black, t = transparent
+    },
 
-	getStyles: function() {
+
+    getStyles: function() {
         return ["MMM-EventHorizon.css"];
     },
 
 
-	getDom: function() {
+    getDom: function() {
 
 
 		var iframe = document.createElement("IFRAME");
@@ -35,23 +47,23 @@ Module.register("MMM-EventHorizon",{
 
 					if (this.config.countUp == "yes" && this.config.size == "small"){
 						// small
-						iframe.src = "http://free.timeanddate.com/countdown/i6f519yg/n179/cf12/cm0/cu4/ct0/cs0/ca0/cr0/ss0/cac0ff/cpc0ff/pc000/tcfff/fs100/szw256/szh108/tat" + this.config.text1 + "/tac0ff/tpt" + this.config.endText1 + "/tpc0ff/mat" + this.config.text2 + "/mac0ff/mpt" + this.config.endText2 + "/mpc0ff/iso" + this.config.date + "T" + this.config.time;
+						iframe.src = "http://free.timeanddate.com/countdown/i6f519yg/" + this.config.timezone + "/cf12/cm0/cu4/ct0/cs0/ca0/cr0/ss0/cac" + this.config.timerColor + "/cpc" + this.config.timerUpColor + "/pc" + this.config.colorpc + "/tcfff/fs100/szw256/szh108/tat" + this.config.text1 + "/tac" + this.config.text1Color + "/tpt" + this.config.endText1 + "/tpc" + this.config.endText1Color + "/mat" + this.config.text2 + "/mac" + this.config.text2Color + "/mpt" + this.config.endText2 + "/mpc" + this.config.endText2Color + "/iso" + this.config.date + "T" + this.config.time;
 					} else if (this.config.countUp == "yes" && this.config.size == "medium") {
 						// medium
-						iframe.src = "http://free.timeanddate.com/countdown/i6f519yg/n179/cf12/cm0/cu4/ct0/cs0/ca0/cr0/ss0/cac0ff/cpc0ff/pc000/tcfff/fs100/szw320/szh135/tat" + this.config.text1 + "/tac0ff/tpt" + this.config.endText1 + "/tpc0ff/mat" + this.config.text2 + "/mac0ff/mpt" + this.config.endText2 + "/mpc0ff/iso" + this.config.date + "T" + this.config.time;
+						iframe.src = "http://free.timeanddate.com/countdown/i6f519yg/" + this.config.timezone + "/cf12/cm0/cu4/ct0/cs0/ca0/cr0/ss0/cac" + this.config.timerColor + "/cpc" + this.config.timerUpColor + "/pc" + this.config.colorpc + "/tcfff/fs100/szw320/szh135/tat" + this.config.text1 + "/tac" + this.config.text1Color + "/tpt" + this.config.endText1 + "/tpc" + this.config.endText1Color + "/mat" + this.config.text2 + "/mac" + this.config.text2Color + "/mpt" + this.config.endText2 + "/mpc" + this.config.endText2Color + "/iso" + this.config.date + "T" + this.config.time;
 					} else if (this.config.countUp == "yes" && this.config.size == "large") {
 					  // large
-						iframe.src = "http://free.timeanddate.com/countdown/i6f519yg/n179/cf12/cm0/cu4/ct0/cs0/ca0/cr0/ss0/cac0ff/cpc0ff/pc000/tcfff/fs100/szw576/szh243/tat" + this.config.text1 + "/tac0ff/tpt" + this.config.endText1 + "/tpc0ff/mat" + this.config.text2 + "/mac0ff/mpt" + this.config.endText2 + "/mpc0ff/iso" + this.config.date + "T" + this.config.time;
+						iframe.src = "http://free.timeanddate.com/countdown/i6f519yg/" + this.config.timezone + "/cf12/cm0/cu4/ct0/cs0/ca0/cr0/ss0/cac" + this.config.timerColor + "/cpc" + this.config.timerUpColor + "/pc" + this.config.colorpc + "/tcfff/fs100/szw576/szh243/tat" + this.config.text1 + "/tac" + this.config.text1Color + "/tpt" + this.config.endText1 + "/tpc" + this.config.endText1Color + "/mat" + this.config.text2 + "/mac" + this.config.text2Color + "/mpt" + this.config.endText2 + "/mpc" + this.config.endText2Color + "/iso" + this.config.date + "T" + this.config.time;
 					}
 
 
 
         if (this.config.countUp != "yes" && this.config.size == "small"){
-            iframe.src = "http://free.timeanddate.com/countdown/i64zeztz/n179/cf100/cm0/cu4/ct5/cs0/ca0/cr0/ss0/cac33d9ff/cpc000/pct/tcfff/fs100/szw192/szh81/tat" + this.config.text1 + "/tac33d9ff/tpt" + this.config.endText1 + "/tpc33d9ff/mat" + this.config.text2 + "/mac33d9ff/mpt" + this.config.endText2 + "/mpc33d9ff/iso" + this.config.date + "T" + this.config.time; // + "/bo2/pd2";
+            iframe.src = "http://free.timeanddate.com/countdown/i64zeztz/" + this.config.timezone + "/cf100/cm0/cu4/ct5/cs0/ca0/cr0/ss0/cac" + this.config.timerColor + "/cpc" + this.config.timerUpColor + "/pc" + this.config.colorpc + "/tcfff/fs100/szw192/szh81/tat" + this.config.text1 + "/tac" + this.config.text1Color + "/tpt" + this.config.endText1 + "/tpc" + this.config.endText1Color + "/mat" + this.config.text2 + "/mac" + this.config.text2Color + "/mpt" + this.config.endText2 + "/mpc" + this.config.endText2Color + "/iso" + this.config.date + "T" + this.config.time; // + "/bo2/pd2";
         } else if (this.config.countUp != "yes" && this.config.size == "medium") {
-            iframe.src = "http://free.timeanddate.com/countdown/i67bgd3d/n179/cf12/cm0/cu4/ct0/cs1/ca0/cr0/ss0/cac0ff/cpc0ff/pct/tcfff/fs100/szw448/szh189/tat" + this.config.text1 + "/tac0ff/tpt" + this.config.endText1 + "/tpc0ff/mat" + this.config.text2 + "/mac0ff/mpt" + this.config.endText2 + "/mpc0ff/iso" + this.config.date + "T" + this.config.time; // + "/bo2/pd2";
+            iframe.src = "http://free.timeanddate.com/countdown/i67bgd3d/" + this.config.timezone + "/cf12/cm0/cu4/ct0/cs1/ca0/cr0/ss0/cac" + this.config.timerColor + "/cpc" + this.config.timerUpColor + "/pc" + this.config.colorpc + "/tcfff/fs100/szw448/szh189/tat" + this.config.text1 + "/tac" + this.config.text1Color + "/tpt" + this.config.endText1 + "/tpc" + this.config.endText1Color + "/mat" + this.config.text2 + "/mac" + this.config.text2Color + "/mpt" + this.config.endText2 + "/mpc" + this.config.endText2Color + "/iso" + this.config.date + "T" + this.config.time; // + "/bo2/pd2";
         } else if (this.config.countUp != "yes" && this.config.size == "large") {
-            iframe.src = "http://free.timeanddate.com/countdown/i67bgd3d/n179/cf12/cm0/cu4/ct0/cs1/ca0/cr0/ss0/cac0ff/cpc0ff/pct/tcfff/fs100/szw576/szh243/tat" + this.config.text1 + "/tac0ff/tpt" + this.config.endText1 + "/tpc0ff/mat" + this.config.text2 + "/mac0ff/mpt" + this.config.endText2 + "/mpc0ff/iso" + this.config.date + "T" + this.config.time; // + "/bo2/pd2";
+            iframe.src = "http://free.timeanddate.com/countdown/i67bgd3d/" + this.config.timezone + "/cf12/cm0/cu4/ct0/cs1/ca0/cr0/ss0/cac" + this.config.timerColor + "/cpc" + this.config.timerUpColor + "/pc" + this.config.colorpc + "/tcfff/fs100/szw576/szh243/tat" + this.config.text1 + "/tac" + this.config.text1Color + "/tpt" + this.config.endText1 + "/tpc" + this.config.endText1Color + "/mat" + this.config.text2 + "/mac" + this.config.text2Color + "/mpt" + this.config.endText2 + "/mpc" + this.config.endText2Color + "/iso" + this.config.date + "T" + this.config.time; // + "/bo2/pd2";
         }
 
 
--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 ## MMM-EventHorizon
 
-Simple countdown timer for events. Birthdays, weddings, anniversaries, vacations, etc.
+Simple countdown timer for events.
 
 ## Examples
 
@@ -16,22 +16,51 @@ Simple countdown timer for events. Birthdays, weddings, anniversaries, vacations
 
 ## Config.js entry and options
 
-```
-    {
+{
         disabled: false,
         module: 'MMM-EventHorizon',
         position: 'bottom_center',
-	    config: {
-            size: "large",                       // small, medium or large
-	    countUp: "yes",                      // Yes or no (when timer ends)
-            date: "2018-07-04",                  // (YYYY-MM-DD)  Date timer will end
-            time: "00:00:01",                    // (HH:MM:SS)    Exact time you want timer to end
-	    text1: "Open the damn pool",         // 1st line of text during timer. About 18 characters total.
-            text2: "Winter sucked ass",          // 2nd line of text during timer. About 18 characters total.
-            endText1: "It's gonna be fun",          // 1st line of text when timer ends. About 18 characters total.
-            endText2: "Where's my bikini",          // 2nd line of text when timer ends. About 18 characters total.
+	          config: {
+            timezone: "n179",                     // See Timezone chart at bottom
+            size: "large",                        // small, medium or large
+            countUp: "yes",                       // Count up after timer ends
+            date: "2019-01-15",                   // YYYY-MM-DD format ONLY
+            time: "00:00:01",                     // (HH:MM:SS)    Exact time you want timer to end
+            text1: "Describe event here",         // 2 lines of text during timer
+            text2: "And here too!",               // 2 lines of text during timer
+            text1Color: "FFFFFF",                 // Hex color codes
+            text2Color: "62FF00",                 // Hex color codes
+            timerColor: "FFFFFF",                 // Hex color codes
+            endText1: "When timer ends",          // 2 lines of text when timer ends
+            endText2: "Say something here",       // 2 lines of text when timer ends
+            endText1Color: "FFFFFF",              // Hex color codes
+            endText2Color: "62FF00",              // Hex color codes
+            timerUpColor: "FFFFFF",               // Hex color codes
+            colorpc: "000",                       // color of the background 000 = black, t = transparent
     }
 },
-```
 
-## Comments welcome
+# Timezone codes - Added as needed. Please ask at the MagicMirror forum
+
+* France, Paris = n195
+
+* Germany, Berlin = n37
+
+* Norway, Oslo = n187
+
+* Sweden, Stockholm = n239
+
+* Switzerland, Geneva = n87
+
+* UK, London = n136
+
+* USA, CA = n137
+* USA, NY = n179
+* USA, IL = n64
+* USA TX = n70
+
+## Special thanks to @spitzlbergerj
+
+For going out of his way and making some nice additions to the module,
+
+Even if he made it work ONLY for Berlin Germany! I kid you not! :-)
